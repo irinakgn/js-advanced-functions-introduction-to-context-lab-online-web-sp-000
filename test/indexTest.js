1e1bb7a602e8b017503675e728b49e1526752a4b
Irina Kagan <irinagamb@gmail.com>
2019-10-27 13:19:37 +0000

    Automatically backed up by Learn

--- test/indexTest.js
+++ test/indexTest.js
@@ -296,7 +296,7 @@ describe("The payroll system", function () {
             expect(calculatePayroll).to.exist
           })
 
-          it.only("correctly sums the payroll burden to $11,880 when passed an array of employee records", function () {
+          it.only("correctly sums the payroll burden to $12,480 when passed an array of employee records", function () {
             let employeeRecords = createEmployeeRecords(csvDataEmployees)
             employeeRecords.forEach(function (rec) {
               let timesInRecordRow = csvTimesIn.find(function (row) {
@@ -315,7 +315,7 @@ describe("The payroll system", function () {
                 createTimeOutEvent(rec, timeOutStamp)
               })
             })
-            expect(calculatePayroll(employeeRecords)).to.eql(11880)
+            expect(calculatePayroll(employeeRecords)).to.eql(12480)
           })
         })
       })
