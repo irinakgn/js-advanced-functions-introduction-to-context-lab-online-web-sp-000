fa1169dc29f4afcac35f95249e13ff14ed547f77
Irina Kagan <irinagamb@gmail.com>
2019-10-27 13:11:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -66,7 +66,7 @@ function allWagesFor(employee){
   const timeOut = employee.timeOutEvents.reduce((total, time)=> { total += time.hour; return total}, 0)
   const hoursWorked = Math.abs(timeIn - timeOut)/100
 
-  console.log(employee, ' ' hoursWorked)
+  console.log(employee, ' ', hoursWorked)
   const wagesEarned =  payPerHour * hoursWorked
 
   return  wagesEarned
